f77b399ed32deef23849e2c2efa1917209e688ec
pbrosamond <pbrosamond@gmail.com>
2024-01-17 22:19:07 -0800

    Video feature completed. Need to redesign controls. Need to set aspect ratio with increasing screen width

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
 import './App.scss';
 
 function App() {
-  return (
-  <Header />
+  return ( <>
+    <Header />
+    <Hero />
+    </>
   );
 }
 
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,11 @@
+import './Hero.scss';
+
+function Hero() {
+    return (
+        <section class="hero">
+            <video class="hero__video" width="100%" height="auto" controls alt="BMX Rampage Video" poster="https://project-2-api.herokuapp.com/images/image0.jpg"></video>
+        </section>
+    );
+  }
+  
+  export default Hero;
