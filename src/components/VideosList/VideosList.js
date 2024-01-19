d012be41816c1c44867c217c74d38333e6170c79
pbrosamond <pbrosamond@gmail.com>
2024-01-19 08:33:54 -0800

    Made few alignment changes overall. Now starting a new branch for functionality.

--- src/components/VideosList/VideosList.js
+++ src/components/VideosList/VideosList.js
@@ -2,87 +2,171 @@ import './VideosList.scss';
 
 function VideosList() {
   return (
-  <section class="videos-list">
-    <div class="videos-list__alignment">
-      <div class="videos-list__container">
-        <img class='videos-list__image' src="https://project-2-api.herokuapp.com/images/image1.jpg" alt="Next Video 1"></img>
-      </div>
-      <div class="videos-list__subcontainer">
-        <p class="videos-list__title">Become A Travel Pro In One Easy Lesson</p>
-        <p class="videos-list__author">Todd Welch</p>
-      </div>
-    </div>
+    <ul class="videos-list">
+      <li class="videos-list__alignment">
+        <div class="videos-list__container">
+          <img class='videos-list__image' src="https://project-2-api.herokuapp.com/images/image1.jpg" alt="Next Video 1"></img>
+        </div>
+        <div class="videos-list__subcontainer">
+          <p class="videos-list__title">Become A Travel Pro In One Easy Lesson</p>
+          <p class="videos-list__author">Todd Welch</p>
+        </div>
+      </li>
 
-    <div class="videos-list__alignment">
-      <div class="videos-list__container">
-        <img class='videos-list__image' src="https://project-2-api.herokuapp.com/images/image2.jpg" alt="Next Video 1"></img>
-      </div>
-      <div class="videos-list__subcontainer">
-        <p class="videos-list__title">Les Houches The Hidden Gem Of The…</p>
-        <p class="videos-list__author">Cornelia Blair</p>
-      </div>
-    </div>
+      <li class="videos-list__alignment">
+        <div class="videos-list__container">
+          <img class='videos-list__image' src="https://project-2-api.herokuapp.com/images/image2.jpg" alt="Next Video 1"></img>
+        </div>
+        <div class="videos-list__subcontainer">
+          <p class="videos-list__title">Become A Travel Pro In One Easy Lesson</p>
+          <p class="videos-list__author">Todd Welch</p>
+        </div>
+      </li>
 
-    <div class="videos-list__alignment">
-      <div class="videos-list__container">
-        <img class='videos-list__image' src="https://project-2-api.herokuapp.com/images/image3.jpg" alt="Next Video 1"></img>
-      </div>
-      <div class="videos-list__subcontainer">
-        <p class="videos-list__title">Travel Health Useful Medical Information…</p>
-        <p class="videos-list__author">Glen Harper</p>
-      </div>
-    </div>
+      <li class="videos-list__alignment">
+        <div class="videos-list__container">
+          <img class='videos-list__image' src="https://project-2-api.herokuapp.com/images/image3.jpg" alt="Next Video 1"></img>
+        </div>
+        <div class="videos-list__subcontainer">
+          <p class="videos-list__title">Become A Travel Pro In One Easy Lesson</p>
+          <p class="videos-list__author">Todd Welch</p>
+        </div>
+      </li>
 
-    <div class="videos-list__alignment">
-      <div class="videos-list__container">
-        <img class='videos-list__image' src="https://project-2-api.herokuapp.com/images/image4.jpg" alt="Next Video 1"></img>
-      </div>
-      <div class="videos-list__subcontainer">
-        <p class="videos-list__title">Cheap Airline Tickets Great Ways To Save</p>
-        <p class="videos-list__author">Emily Harper</p>
-      </div>
-    </div>
+      <li class="videos-list__alignment">
+        <div class="videos-list__container">
+          <img class='videos-list__image' src="https://project-2-api.herokuapp.com/images/image4.jpg" alt="Next Video 1"></img>
+        </div>
+        <div class="videos-list__subcontainer">
+          <p class="videos-list__title">Become A Travel Pro In One Easy Lesson</p>
+          <p class="videos-list__author">Todd Welch</p>
+        </div>
+      </li>
 
-    <div class="videos-list__alignment">
-      <div class="videos-list__container">
-        <img class='videos-list__image' src="https://project-2-api.herokuapp.com/images/image5.jpg" alt="Next Video 1"></img>
-      </div>
-      <div class="videos-list__subcontainer">
-        <p class="videos-list__title">Take A Romantic Break In A Boutique…</p>
-        <p class="videos-list__author">Ethan Owen</p>
-      </div>
-    </div>
+      <li class="videos-list__alignment">
+        <div class="videos-list__container">
+          <img class='videos-list__image' src="https://project-2-api.herokuapp.com/images/image5.jpg" alt="Next Video 1"></img>
+        </div>
+        <div class="videos-list__subcontainer">
+          <p class="videos-list__title">Become A Travel Pro In One Easy Lesson</p>
+          <p class="videos-list__author">Todd Welch</p>
+        </div>
+      </li>
 
-    <div class="videos-list__alignment">
-      <div class="videos-list__container">
-        <img class='videos-list__image' src="https://project-2-api.herokuapp.com/images/image6.jpg" alt="Next Video 1"></img>
-      </div>
-      <div class="videos-list__subcontainer">
-        <p class="videos-list__title">Choose The Perfect Accomodations</p>
-        <p class="videos-list__author">Lydia Perez</p>
-      </div>
-    </div>
+      <li class="videos-list__alignment">
+        <div class="videos-list__container">
+          <img class='videos-list__image' src="https://project-2-api.herokuapp.com/images/image6.jpg" alt="Next Video 1"></img>
+        </div>
+        <div class="videos-list__subcontainer">
+          <p class="videos-list__title">Become A Travel Pro In One Easy Lesson</p>
+          <p class="videos-list__author">Todd Welch</p>
+        </div>
+      </li>
 
-    <div class="videos-list__alignment">
-      <div class="videos-list__container">
-        <img class='videos-list__image' src="https://project-2-api.herokuapp.com/images/image7.jpg" alt="Next Video 1"></img>
-      </div>
-      <div class="videos-list__subcontainer">
-        <p class="videos-list__title">Cruising Destination Ideas</p>
-        <p class="videos-list__author">Timothy Austin</p>
-      </div>
-    </div>
+      <li class="videos-list__alignment">
+        <div class="videos-list__container">
+          <img class='videos-list__image' src="https://project-2-api.herokuapp.com/images/image7.jpg" alt="Next Video 1"></img>
+        </div>
+        <div class="videos-list__subcontainer">
+          <p class="videos-list__title">Become A Travel Pro In One Easy Lesson</p>
+          <p class="videos-list__author">Todd Welch</p>
+        </div>
+      </li>
 
-    <div class="videos-list__alignment">
-      <div class="videos-list__container">
-        <img class='videos-list__image' src="https://project-2-api.herokuapp.com/images/image8.jpg" alt="Next Video 1"></img>
-      </div>
-      <div class="videos-list__subcontainer">
-        <p class="videos-list__title">Train Travel On Track For Safety</p>
-        <p class="videos-list__author">Scotty Cramer</p>
-      </div>
-    </div>
-  </section>
+      <li class="videos-list__alignment">
+        <div class="videos-list__container">
+          <img class='videos-list__image' src="https://project-2-api.herokuapp.com/images/image8.jpg" alt="Next Video 1"></img>
+        </div>
+        <div class="videos-list__subcontainer">
+          <p class="videos-list__title">Become A Travel Pro In One Easy Lesson</p>
+          <p class="videos-list__author">Todd Welch</p>
+        </div>
+      </li>
+    </ul>
+
+
+
+  // <section class="videos-list">
+  //   <div class="videos-list__alignment">
+      // <div class="videos-list__container">
+      //   <img class='videos-list__image' src="https://project-2-api.herokuapp.com/images/image1.jpg" alt="Next Video 1"></img>
+      // </div>
+      // <div class="videos-list__subcontainer">
+      //   <p class="videos-list__title">Become A Travel Pro In One Easy Lesson</p>
+      //   <p class="videos-list__author">Todd Welch</p>
+      // </div>
+  //   </div>
+
+  //   <div class="videos-list__alignment">
+  //     <div class="videos-list__container">
+  //       <img class='videos-list__image' src="https://project-2-api.herokuapp.com/images/image2.jpg" alt="Next Video 1"></img>
+  //     </div>
+  //     <div class="videos-list__subcontainer">
+  //       <p class="videos-list__title">Les Houches The Hidden Gem Of The…</p>
+  //       <p class="videos-list__author">Cornelia Blair</p>
+  //     </div>
+  //   </div>
+
+  //   <div class="videos-list__alignment">
+  //     <div class="videos-list__container">
+  //       <img class='videos-list__image' src="https://project-2-api.herokuapp.com/images/image3.jpg" alt="Next Video 1"></img>
+  //     </div>
+  //     <div class="videos-list__subcontainer">
+  //       <p class="videos-list__title">Travel Health Useful Medical Information…</p>
+  //       <p class="videos-list__author">Glen Harper</p>
+  //     </div>
+  //   </div>
+
+  //   <div class="videos-list__alignment">
+  //     <div class="videos-list__container">
+  //       <img class='videos-list__image' src="https://project-2-api.herokuapp.com/images/image4.jpg" alt="Next Video 1"></img>
+  //     </div>
+  //     <div class="videos-list__subcontainer">
+  //       <p class="videos-list__title">Cheap Airline Tickets Great Ways To Save</p>
+  //       <p class="videos-list__author">Emily Harper</p>
+  //     </div>
+  //   </div>
+
+  //   <div class="videos-list__alignment">
+  //     <div class="videos-list__container">
+  //       <img class='videos-list__image' src="https://project-2-api.herokuapp.com/images/image5.jpg" alt="Next Video 1"></img>
+  //     </div>
+  //     <div class="videos-list__subcontainer">
+  //       <p class="videos-list__title">Take A Romantic Break In A Boutique…</p>
+  //       <p class="videos-list__author">Ethan Owen</p>
+  //     </div>
+  //   </div>
+
+  //   <div class="videos-list__alignment">
+  //     <div class="videos-list__container">
+  //       <img class='videos-list__image' src="https://project-2-api.herokuapp.com/images/image6.jpg" alt="Next Video 1"></img>
+  //     </div>
+  //     <div class="videos-list__subcontainer">
+  //       <p class="videos-list__title">Choose The Perfect Accomodations</p>
+  //       <p class="videos-list__author">Lydia Perez</p>
+  //     </div>
+  //   </div>
+
+  //   <div class="videos-list__alignment">
+  //     <div class="videos-list__container">
+  //       <img class='videos-list__image' src="https://project-2-api.herokuapp.com/images/image7.jpg" alt="Next Video 1"></img>
+  //     </div>
+  //     <div class="videos-list__subcontainer">
+  //       <p class="videos-list__title">Cruising Destination Ideas</p>
+  //       <p class="videos-list__author">Timothy Austin</p>
+  //     </div>
+  //   </div>
+
+  //   <div class="videos-list__alignment">
+  //     <div class="videos-list__container">
+  //       <img class='videos-list__image' src="https://project-2-api.herokuapp.com/images/image8.jpg" alt="Next Video 1"></img>
+  //     </div>
+  //     <div class="videos-list__subcontainer">
+  //       <p class="videos-list__title">Train Travel On Track For Safety</p>
+  //       <p class="videos-list__author">Scotty Cramer</p>
+  //     </div>
+  //   </div>
+  // </section>
   
   );
 }
